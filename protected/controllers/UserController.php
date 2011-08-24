90899b2bd43eab877563906061802fb2cb6ed950
Роман <megafon-don@mail.ru>
2011-08-25 00:48:11 +0400

    OpenID support.

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -195,9 +195,37 @@ class UserController extends Controller
 		    }
 		}
 		
+		$login_form = new Users('login');
+		if(isset($_POST['User'])){
+            $login_form->attributes = $_POST['User'];
+			if($login_form->validate() && $user_openid['identity']){
+				if($login_form->attachOidIdentity($user_openid['identity'], Yii::app()->user->id)){
+					Yii::app()->session->offsetUnset('user_openid');
+					$this->redirect(array('main/index'));
+				}
+			}
+		}
+		$reg_form = new Users('register_oid');
+		$reg_form->username = @$user_openid['username'];
+		$reg_form->email = @$user_openid['email'];
+		if(isset($_POST['NewUser'])){
+            $reg_form->attributes = $_POST['NewUser'];
+			$pass = md5(time().mt_rand(0, 1).$reg_form->attributes['username']);
+			$reg_form->password = $reg_form->password2 = $pass;
+			if(@$user_openid['identity'] && $reg_form->save()){
+				if(Users::authenticateById($reg_form->userID)){
+				    $reg_form->attachOidIdentity($user_openid['identity'], $reg_form->userID);
+					Yii::app()->session->offsetUnset('user_openid');
+					$this->redirect(array('/user'));	
+				}
+			}
+        }
+		
 		$this->render('openid', array(
             'userinfo' => $user_openid,
-			'error' => $err
+			'error' => $err,
+			'login_form' => $login_form,
+			'reg_form' => $reg_form
 		));
 	}
 
--- protected/models/Users.php
+++ protected/models/Users.php
@@ -49,7 +49,7 @@ class Users extends CActiveRecord
 		return array(
             // Общие
             array('username, password', 'required', 'message' => '{attribute} не может быть пустым'),
-            array('username', 'match', 'pattern' => '/^[A-Za-z0-9А-Яа-я\s-]+$/i','message' => 'Логин содержит недопустимые символы'),
+            array('username', 'match', 'pattern' => '/^[A-Za-z0-9А-Яа-я\s-_]+$/i','message' => 'Логин содержит недопустимые символы'),
             array('email', 'email', 'message' => '{attribute} имеет недопустимый формат'),
             array('username', 'length', 'max'=>32, 'min'=>3, 'message' => 'Длина логина - от 3 до 32 символов'),
             array('password', 'length', 'max'=>32, 'min'=>3, 'message' => 'Длина пароля - от 3 до 32 символов'),
@@ -57,9 +57,9 @@ class Users extends CActiveRecord
             // Авторизация
             array('password', 'authenticate', 'on' => 'login'),
             // Регистрация
-            array('username, email', 'unique', 'message' => 'Указанный {attribute} уже используется в системе','on' => 'register'),
-            array('email', 'required','on' => 'register', 'message' => ' Не указан {attribute}'),
-            array('password', 'compare', 'compareAttribute'=>'password2', 'on'=>'register', 'message' => 'Пароли не совпадают'),
+            array('username, email', 'unique', 'message' => 'Указанный {attribute} уже используется в системе','on' => 'register, register_oid'),
+            array('email', 'required','on' => 'register,register_oid', 'message' => ' Не указан {attribute}'),
+            array('password', 'compare', 'compareAttribute'=>'password2', 'on'=>'register,register_oid', 'message' => 'Пароли не совпадают'),
             array('captcha', 'captcha', 'allowEmpty'=>!extension_loaded('gd'), 'on' => 'register', 'message' => 'Неверный код подтверждения'),
             array('password2', 'safe')
 		);
@@ -148,6 +148,14 @@ class Users extends CActiveRecord
         return true;
 	}
 	
+	public static function authenticateById($id){
+        $identity= new UserIdentity(null, null);
+        $identity->authenticateById($id);
+        if($identity->errorCode != UserIdentity::ERROR_NONE) return false;  
+        Yii::app()->user->login($identity, 60*60*24*365);
+        return true;
+    }
+	
 	
 	public static function authenticateByOidIdentity($identity){
 		$id = Yii::app()->db->createCommand()
@@ -163,6 +171,13 @@ class Users extends CActiveRecord
             return true;
         }
         return false;
+	}
+	
+	public function attachOidIdentity($identity, $user_id){
+        return Yii::app()->db->createCommand()->insert('users_openid', array(
+            'identity' => $identity,
+            'user_id' => $user_id,
+        ));
 	}
 }
 
