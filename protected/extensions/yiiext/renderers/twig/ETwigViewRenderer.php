3ec9f202ab724063db213da4bb05388c577b5359
Роман <megafon-don@mail.ru>
2011-09-02 22:57:08 +0400

    Some mods for Twig-ext

--- protected/extensions/yiiext/renderers/twig/ETwigViewRenderer.php
+++ protected/extensions/yiiext/renderers/twig/ETwigViewRenderer.php
@@ -22,6 +22,8 @@ class ETwigViewRenderer extends CApplicationComponent implements IViewRenderer {
 	 * @var array extension classes list
 	 */
 	public $extentions = array();
+	
+	public $register_static = array();
 
     private $twig;
 
@@ -58,6 +60,10 @@ class ETwigViewRenderer extends CApplicationComponent implements IViewRenderer {
                 $this->twig->addExtension(new $extName());
             }
     	}
+		// Register statics
+		foreach ($this->register_static as $s) {
+            $this->twig->addGlobal($s, new ETwigStaticClassProxy($s));	
+		}
     }
 
     /**
@@ -98,3 +104,28 @@ class ETwigViewRenderer extends CApplicationComponent implements IViewRenderer {
 		return Yii::app()->theme->getBasePath();
 	}
 }
+
+
+class ETwigStaticClassProxy
+{
+    private $_staticClassName;
+
+    public function __construct($staticClassName) {
+        $this->_staticClassName = $staticClassName;
+    }
+
+    public function __get($property)
+    {
+        return ${$this->_staticClassName}::$property;
+    }
+
+    public function __set($property, $value)
+    {
+        return (${$this->_staticClassName}::$property = $value);
+    }
+
+    public function __call($method, $arguments)
+    {
+        return call_user_func_array(array($this->_staticClassName, $method), $arguments);
+    }
+} 
