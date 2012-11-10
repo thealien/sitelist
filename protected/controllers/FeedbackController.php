a598fd934c38bfb82becc27f749dcd47a93e0933
thealien <roman.ziva@gmail.com>
2012-11-10 23:42:24 +0400

    Remove POST handler from Feedback action

--- protected/controllers/FeedbackController.php
+++ protected/controllers/FeedbackController.php
@@ -4,19 +4,6 @@ class FeedbackController extends Controller
 {
     public function actionIndex(){
         $model = new FeedbackForm();
-		if(isset($_POST['FeedbackForm'])){
-            $model->attributes = $_POST['FeedbackForm'];
-            if($model->validate()){
-                if(MailHelper::sendFeedback($model->email, $model->text)){
-                	Yii::app()->user->setFlash('feedback',true);
-					$this->refresh();
-                }
-                else{
-                	$model->addError('text', 'Ошибка отправки, попробуйте позже.');
-                }
-            }   
-        }
-
         $lastLinks = Links::getLastLinks(2);
         Yii::app()->params['title'] = 'Обратная связь — ' . Yii::app()->params['title'];
         $this->render('index', array(
